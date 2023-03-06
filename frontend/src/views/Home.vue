2a7c92786a87101c85ed8eb6e05b13263a9ec147
rcidral <ricidralmfo@gmail.com>
2023-03-05 21:19:55 -0300

    starting dashboard

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,13 @@
+<script>
+import Home from "../components/Home.vue";
+export default {
+  components: { Home },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+    <Home />
+</template>
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  };
